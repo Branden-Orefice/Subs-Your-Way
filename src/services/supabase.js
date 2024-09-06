1028a5b30e942846ff80fa8481af6be3e62794e0
Branden Orefice <Bran.Orefice@gmail.com>
2024-09-05 23:05:04 -0400

    display supabase key

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseUrl = "https://iiwmpwbdsfvaswnxdoyt.supabase.co"
-const supabaseKey = import.meta.env.VITE_APP_SUPABASE_KEY
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlpd21wd2Jkc2Z2YXN3bnhkb3l0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM0MDg2NjEsImV4cCI6MjAzODk4NDY2MX0.HqLUS5k7DXdRqqwdkL4MVW1KedOiHD-RWYv8GFuDFAM"
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
